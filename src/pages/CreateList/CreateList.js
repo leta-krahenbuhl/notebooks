18033698abac052d366fdb503d2a85369a6fa71e
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2023-11-16 14:23:37 +0000

    Rename elements, add / delete / submit items

--- src/pages/CreateList/CreateList.js
+++ src/pages/CreateList/CreateList.js
@@ -1,89 +1,96 @@
 import "./CreateList.scss";
 import { useState, useRef } from "react";
-import CreateListInput from "../../components/CreateListInput/CreateListInput";
+import ListItemInput from "../../components/ListItemInput/ListItemInput";
 
 export default function CreateList() {
-  const [formValues, setFormValues] = useState([]);
+  const [listItems, setlistItems] = useState([]);
   const [toggle, setToggle] = useState(false);
 
-  const inputRef = useRef();
-  const selectRef = useRef();
+  const inputRefItem = useRef();
+  const checkboxRef = useRef();
+  //   const selectRef = useRef();
 
-  const handleChange = (e, index) => {
-    const values = [...formValues];
-    values[index].value = e.target.value;
-    setFormValues(values);
+  const handleChange = (event, index) => {
+    const values = [...listItems];
+    values[index].value = event.target.value;
+    setlistItems(values);
   };
 
-  const handleAddField = (e) => {
-    e.preventDefault();
-    const values = [...formValues];
-    values.push({
-      label: inputRef.current.value || "label",
-      type: selectRef.current.value || "text",
-      value: "",
+  const handleAddToList = (event) => {
+    event.preventDefault();
+    const listItemsCurrent = [...listItems];
+    listItemsCurrent.push({
+      //: TO DO validation here if list item empty
+      //   type: selectRef.current.value || "text",
+      value: inputRefItem.current.value,
     });
-    setFormValues(values);
+    setlistItems(listItemsCurrent);
     setToggle(false);
   };
 
-  const handleDeleteField = (e, index) => {
-    const values = [...formValues];
+  const handleDeleteField = (_event, index) => {
+    const values = [...listItems];
     values.splice(index, 1);
-    setFormValues(values);
+    setlistItems(values);
   };
 
-  const addBtnClick = (e) => {
-    e.preventDefault();
+  const handleAddListItemField = (event) => {
+    event.preventDefault();
+
     setToggle(true);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     console.log(
-      formValues.map((val) => {
-        return { [val.label]: val.value };
+      listItems.map((item) => {
+        return item.value;
       })
     );
+    setlistItems([]);
   };
 
   return (
     // changed class name of div from App to create-list
     // use type checkbox once it's working
     <div className="create-list">
-      <h2 className="create-list__title">Add a new list</h2>
+      <h2 className="create-list__title">ADD LIST</h2>
       <form onSubmit={handleSubmit}>
-        {formValues.map((obj, index) => (
-          <CreateListInput
+        {listItems.map((item, index) => (
+          <ListItemInput
             key={index}
-            objValue={obj}
+            itemValue={item}
             onChange={handleChange}
             index={index}
-            deleteField={(e) => handleDeleteField(e, index)}
+            deleteField={(event) => handleDeleteField(event, index)}
           />
         ))}
+        {/* ADD LIST ITEM button shows up when toggle is false
+        eg no one clicked on add list item.
+        If toggle is true someone clicked on ADD LIST ITEM BUTTON
+        so we see the input field
+        */}
         {!toggle ? (
           <div className="center">
-            <button className="add-btn" onClick={addBtnClick}>
-              Add new
+            <button className="add-btn" onClick={handleAddListItemField}>
+              ADD LIST ITEM
             </button>
           </div>
         ) : (
           <div className="dialog-box">
-            <input type="text" placeholder="label" ref={inputRef} />
-            <select ref={selectRef}>
+            <input type="text" placeholder="item" ref={inputRefItem} />
+            {/* <input type="checkbox" placeholder="checkbox?" ref={checkboxRef} /> */}
+
+            {/* <select ref={selectRef}>
               <option value="text">Text</option>
-              <option value="number">Number</option>
-              <option value="email">Email</option>
-              <option value="password">Password</option>
-            </select>
-            <button className="add-btn" onClick={handleAddField}>
-              Add
+            </select> */}
+            <button className="add-btn" onClick={handleAddToList}>
+              ADD TO LIST
             </button>
           </div>
         )}
         <button type="submit" className="submit-btn">
-          Submit
+          SUBMIT
         </button>
       </form>
     </div>
