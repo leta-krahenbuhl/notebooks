5f701381004a567ad067b3e5b5919e5f835febcd
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2023-11-20 11:23:03 +0000

    Line height of list detail

--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -9,13 +9,17 @@ export default function ListItems({ itemsForTitles }) {
       <div className="list-items">
         {itemsForTitles.map((titleObj, index) => (
           <div key={index}>
-            <ul className="list__text">
+            <ul className="list-items__text">
               {titleObj.items
                 .filter((item) => {
                   return item.list_id === parseInt(listId);
                 })
                 .map((item, itemIndex) => {
-                  return <li key={itemIndex}>{item.text}</li>;
+                  return (
+                    <li key={itemIndex} className="list-items__item">
+                      {item.text}
+                    </li>
+                  );
                 })}
             </ul>
           </div>
