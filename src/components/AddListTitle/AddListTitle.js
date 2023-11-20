7c66083b31d677ecb7f274db5996cb5c9d2f2e6a
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2023-11-20 21:31:00 +0000

    Edit list title functionality works

--- src/components/AddListTitle/AddListTitle.js
+++ src/components/AddListTitle/AddListTitle.js
@@ -37,7 +37,7 @@ export default function AddListTitle() {
 
       getTitles();
     }
-  }, []);
+  }, [title]);
 
   const handleSubmitTitle = async (event) => {
     event.preventDefault();
@@ -64,8 +64,6 @@ export default function AddListTitle() {
         setTitle(newListTitle);
         setIsTitle(true);
 
-        // Update the URL
-        // navigate(`/lists/add/${newListTitleId}`); OLD
         navigate(`/notebooks/${notebookId}/create/list/${newListTitleId}`);
 
         event.target.reset();
@@ -102,6 +100,7 @@ export default function AddListTitle() {
   };
 
   const handleClick = () => {
+    // setTitle(titleArr[0].title);
     setIsTitle(false);
   };
 
@@ -118,7 +117,7 @@ export default function AddListTitle() {
               name="text"
               placeholder="add list title"
               className="add-list-title-form__input"
-              defaultValue={title.title}
+              // defaultValue={title}
             />
             <button className="add-list-title-form__button"></button>
           </div>
@@ -128,14 +127,10 @@ export default function AddListTitle() {
   }
 
   if (!isTitle && listId) {
-    console.log("we have no title but we have list id");
-    //that shouldn't be possible??
-    //need to set title when page loads if there is one... put it in state with axios call?
-
     return (
       <>
         <form className="add-list-title-form" onSubmit={handleSubmitTitle}>
-          <h2 className="add-list-title-form__header">ADD LIST</h2>
+          <h2 className="add-list-title-form__header">EDIT LIST</h2>
           <div className="add-list-title-form__wrapper">
             <input
               type="text"
@@ -143,7 +138,9 @@ export default function AddListTitle() {
               name="text"
               placeholder="add list title"
               className="add-list-title-form__input"
-              defaultValue={title}
+              defaultValue={
+                titleArr && titleArr.length > 0 ? titleArr[0].title : ""
+              }
             />
             <button className="add-list-title-form__button"></button>
           </div>
@@ -153,16 +150,15 @@ export default function AddListTitle() {
   }
 
   if (isTitle) {
-    console.log("title!");
     return (
       <>
-        <h2 className="add-list-title-form__header">ADD LIST</h2>
+        <h2 className="add-list-title-form__header">EDIT LIST</h2>
         <article className="edit-form">
           <h3 className="add-list-title-form__list-title">
-            {titleArr[0].title}
+            {titleArr && titleArr.length > 0 ? titleArr[0].title : ""}
           </h3>
           <img
-            onClick={handleClick}
+            onClick={() => handleClick(titleArr[0].title)}
             className="edit-list-title-form__button"
           ></img>
         </article>
@@ -170,81 +166,3 @@ export default function AddListTitle() {
     );
   }
 }
-
-//------------------------------------ WORKS FOR ADDING
-
-// import "./AddListTitle.scss";
-// import axios from "axios";
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// export default function AddListTitle({ notebookId }) {
-//   const [isTitle, setIsTitle] = useState(false);
-//   const [title, setTitle] = useState("");
-//   const navigate = useNavigate();
-
-//   const handleSubmitTitle = async (event) => {
-//     event.preventDefault();
-
-//     const parsedNotebookId = parseInt(notebookId);
-//     // console.log(parsedNotebookId); //works
-
-//     const newListTitle = {
-//       title: event.target.text.value,
-//       notebook_id: parsedNotebookId,
-//     };
-//     // put front-end form evaluation here
-
-//     const baseURL = process.env.REACT_APP_BASE_URL;
-
-//     try {
-//       const response = await axios.post(
-//         `${baseURL}/api/list-titles`,
-//         newListTitle
-//       );
-//       const newListTitleId = response.data.id;
-
-//       setIsTitle(true);
-//       setTitle(newListTitle);
-
-//       // Update the URL
-//       // navigate(`/lists/add/${newListTitleId}`); OLD
-//       navigate(`/notebooks/${notebookId}/create/list/${newListTitleId}`);
-
-//       event.target.reset();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   if (!isTitle) {
-//     return (
-//       <>
-//         <form className="add-list-title-form" onSubmit={handleSubmitTitle}>
-//           <h2 className="add-list-title-form__header">ADD LIST</h2>
-//           {/* <label htmlFor="text">Title</label> */}
-//           <div className="add-list-title-form__wrapper">
-//             <input
-//               type="text"
-//               id="text"
-//               name="text"
-//               placeholder="add list title"
-//               className="add-list-title-form__input"
-//             />
-//             {/* </div> */}
-//             <button className="add-list-title-form__button"></button>
-//           </div>
-//         </form>
-//       </>
-//     );
-//   }
-
-//   if (isTitle) {
-//     return (
-//       <>
-//         <h2 className="add-list-title-form__header">ADD LIST</h2>
-//         <h3 className="add-list-title-form__list-title">{title.title}</h3>
-//       </>
-//     );
-//   }
-// }
