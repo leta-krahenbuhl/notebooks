0499c67577e8aa453a0b617806c585ec1edc804f
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2023-11-21 16:08:59 +0000

    Header and top nav sticky

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import "./Header.scss";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
   return (
     <header className="header">
       <Link to="/">
         <h1 className="header__logo">Notebooks</h1>
       </Link>
+      {location.pathname === "/" && <div className="header__border"></div>}
     </header>
   );
 }
