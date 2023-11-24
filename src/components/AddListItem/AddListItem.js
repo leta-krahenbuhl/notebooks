ec8869a1479b6f79c7c73981c588a1609b8a96cc
Leta Krahenbuhl <leta.krahenbuhl@gmail.com>
2023-11-24 13:10:43 +0000

    Deleting list item works

--- src/components/AddListItem/AddListItem.js
+++ src/components/AddListItem/AddListItem.js
@@ -6,8 +6,6 @@ import { fetchListItems } from "../../utils/AxiosRequests";
 import { editListItem } from "../../utils/AxiosRequests";
 import { deleteItem } from "../../utils/AxiosRequests"; //move this into Item component
 
-import { useLocation } from "react-router-dom";
-
 export default function AddListItem() {
   const [allItems, setAllItems] = useState([]); //only items for current list title
   const { listId } = useParams();
@@ -16,20 +14,21 @@ export default function AddListItem() {
   const parsedListId = parseInt(listId);
   const baseURL = process.env.REACT_APP_BASE_URL;
 
+  const getItems = async () => {
+    const data = await fetchListItems();
+    const currentItemArr = data.filter((itemObj) => {
+      return itemObj.list_id === parseInt(listId);
+    });
+    return currentItemArr;
+  };
+
   useEffect(() => {
-    const getItems = async () => {
-      const data = await fetchListItems();
-      const currentItemArr = data.filter((itemObj) => {
-        return itemObj.list_id === parseInt(listId);
-      });
-      return currentItemArr;
-    };
     getItems()
       .then((data) => {
         setAllItems(data);
       })
       .catch(console.error);
-  }, []);
+  }, [getItems]);
 
   const handleUpateItem = async (updatedItem) => {
     await editListItem(updatedItem);
@@ -76,11 +75,14 @@ export default function AddListItem() {
       <ul className="add-list-items__list">
         {allItems.map((item) => (
           <li key={item.id} className="add-list-items__item">
-            <Item item={item} onItemUpdate={handleUpateItem} />
+            <Item
+              item={item}
+              onItemUpdate={handleUpateItem}
+              getItems={getItems}
+            />
           </li>
         ))}
       </ul>
-
       <form className="add-list-items-form" onSubmit={handleSubmitItem}>
         <div className="add-list-items-form__wrapper">
           <input
@@ -111,7 +113,7 @@ export default function AddListItem() {
   );
 }
 
-function Item({ item, onItemUpdate }) {
+function Item({ item, onItemUpdate, getItems }) {
   const [isEditing, setIsEditing] = useState(false);
   const [value, setValue] = useState(item.text);
   const [isError, setIsError] = useState(false);
@@ -140,7 +142,8 @@ function Item({ item, onItemUpdate }) {
       } catch (error) {
         console.error(error);
       }
-      // getItems();
+      // console.log("above getItems...");
+      getItems();
     } else {
       //change this before submission
       console.log("Item deletion cancelled");
@@ -181,6 +184,190 @@ function Item({ item, onItemUpdate }) {
     </>
   );
 }
+
+//----------- before taking getItems out of useEffect
+
+// import "./AddListItem.scss";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { fetchListItems } from "../../utils/AxiosRequests";
+// import { editListItem } from "../../utils/AxiosRequests";
+// import { deleteItem } from "../../utils/AxiosRequests"; //move this into Item component
+
+// export default function AddListItem() {
+//   const [allItems, setAllItems] = useState([]); //only items for current list title
+//   const { listId } = useParams();
+//   const [isErrorNewItem, setIsErrorNewItem] = useState(false);
+
+//   const parsedListId = parseInt(listId);
+//   const baseURL = process.env.REACT_APP_BASE_URL;
+
+//   useEffect(() => {
+//     const getItems = async () => {
+//       const data = await fetchListItems();
+//       const currentItemArr = data.filter((itemObj) => {
+//         return itemObj.list_id === parseInt(listId);
+//       });
+//       return currentItemArr;
+//     };
+//     getItems()
+//       .then((data) => {
+//         setAllItems(data);
+//       })
+//       .catch(console.error);
+//   }, []);
+
+//   const handleUpateItem = async (updatedItem) => {
+//     await editListItem(updatedItem);
+//   };
+
+//   // add new list item
+//   const handleSubmitItem = async (event) => {
+//     event.preventDefault();
+
+//     setIsErrorNewItem(false);
+
+//     if (!event.target.listItem.value) {
+//       return setIsErrorNewItem(true);
+//     }
+
+//     if (!listId) {
+//       return alert("Please add a list title before adding list items.");
+//     }
+
+//     const newListItem = {
+//       text: event.target.listItem.value,
+//       list_id: parsedListId,
+//     };
+
+//     try {
+//       const response = await axios.post(
+//         `${baseURL}/api/list-items`,
+//         newListItem
+//       );
+//       //without this I get a "each child has to have a uniuqe key error msg?
+//       const updatedItem = response.data;
+
+//       setIsErrorNewItem(false);
+//       setAllItems((prevItems) => [...prevItems, updatedItem]);
+
+//       event.target.reset();
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   return (
+//     <div className="add-list-items">
+//       <ul className="add-list-items__list">
+//         {allItems.map((item) => (
+//           <li key={item.id} className="add-list-items__item">
+//             <Item item={item} onItemUpdate={handleUpateItem} />
+//           </li>
+//         ))}
+//       </ul>
+
+//       <form className="add-list-items-form" onSubmit={handleSubmitItem}>
+//         <div className="add-list-items-form__wrapper">
+//           <input
+//             type="text"
+//             className={`${
+//               listId
+//                 ? "add-list-items-form__input"
+//                 : "add-list-items-form__input--inactive"
+//             }`}
+//             name="listItem"
+//             placeholder="add list item"
+//           />
+//           <button
+//             className={`${
+//               listId
+//                 ? "add-list-items-form__button"
+//                 : "add-list-items-form__button--inactive"
+//             }`}
+//           ></button>
+//         </div>
+//         {isErrorNewItem && (
+//           <p className="add-notebook-form__error">
+//             Please enter text for your item.
+//           </p>
+//         )}
+//       </form>
+//     </div>
+//   );
+// }
+
+// function Item({ item, onItemUpdate }) {
+//   const [isEditing, setIsEditing] = useState(false);
+//   const [value, setValue] = useState(item.text);
+//   const [isError, setIsError] = useState(false);
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     if (!value) {
+//       return setIsError(true);
+//     }
+
+//     onItemUpdate({ id: item.id, text: value });
+//     setIsEditing(false);
+//     setIsError(false);
+//   };
+
+//   const handleDeleteItem = async () => {
+//     const confirmDelete = window.confirm(
+//       "Are you sure you want to delete this item? This action cannot be undone."
+//     );
+
+//     if (confirmDelete) {
+//       try {
+//         const id = item.id;
+//         await deleteItem(id);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//       // getItems();
+//     } else {
+//       //change this before submission
+//       console.log("Item deletion cancelled");
+//     }
+//   };
+
+//   return (
+//     <>
+//       {isEditing ? (
+//         <form onSubmit={handleSubmit}>
+//           <input
+//             type="text"
+//             value={value}
+//             onChange={(event) => {
+//               setValue(event.target.value);
+//             }}
+//           />
+//           <button className="save-edit-list-item-button"></button>
+//           {isError && (
+//             <p className="add-notebook-form__error">
+//               Please enter text for your item.
+//             </p>
+//           )}
+//         </form>
+//       ) : (
+//         <div>
+//           <p>{value}</p>
+//           <button
+//             className="edit-list-items-button"
+//             onClick={() => setIsEditing(true)}
+//           ></button>
+//           <button
+//             className="delete-list-item-button"
+//             onClick={handleDeleteItem}
+//           ></button>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
 
 //----------- works but when not edited before submitting edit it uses last default value
 // import "./AddListItem.scss";
