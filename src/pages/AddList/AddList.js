84c451f166ce9dc60e2f150596c17662ca293d6f
Leta Krähenbühl <145375943+leta-krahenbuhl@users.noreply.github.com>
2024-07-12 15:21:25 +0100

    Start tot change AddList.js structure

--- src/pages/AddList/AddList.js
+++ src/pages/AddList/AddList.js
@@ -12,41 +12,40 @@ export default function AddList() {
   const location = useLocation();
 
   return (
-    <div className="add-edit-list">
-      <div className="add-edit-list__wrapper">
-        <div className="add-edit-list__wrapper-side">
-          <Header />
-          <nav className="notebook2__nav">
-            <NotebookTitles />
-          </nav>
-          <BottomNavigation />
-        </div>
-        <div className="add-edit-list__wrapper-main">
-          <AddEditListTitle />
-          <AddEditListItems />
-
-          {/* Adding a new list without saving a title => coming from notebook page */}
-          {location.pathname.endsWith("create/lists") && (
-            <Link to={`/notebooks/${notebookId}`}>
-              <button className="add-edit-list__done-button">DONE</button>
-            </Link>
-          )}
-
-          {/* Creating a new list and added title/items and coming from notebook page */}
-          {location.pathname.endsWith(`create/lists/${listId}`) && (
-            <Link to={`/notebooks/${notebookId}`}>
-              <button className="add-edit-list__done-button">DONE</button>
-            </Link>
-          )}
-
-          {/* Edit an existing list => coming from list page*/}
-          {location.pathname.endsWith("/edit") && (
-            <Link to={`/notebooks/${notebookId}/lists/${listId}`}>
-              <button className="add-edit-list__done-button">DONE</button>
-            </Link>
-          )}
-        </div>
+    <article className="add-list">
+      <div className="add-list__nav-wrapper">
+        <Header />
+        <nav className="add-list__nav">
+          <NotebookTitles />
+        </nav>
+        <BottomNavigation />
       </div>
-    </div>
+
+      <main className="add-list__main">
+        <AddEditListTitle />
+        <AddEditListItems />
+
+        {/* Adding a new list without saving a title => coming from notebook page */}
+        {location.pathname.endsWith("create/lists") && (
+          <Link to={`/notebooks/${notebookId}`}>
+            <button className="add-list__done-button">DONE</button>
+          </Link>
+        )}
+
+        {/* Creating a new list and added title/items and coming from notebook page */}
+        {location.pathname.endsWith(`create/lists/${listId}`) && (
+          <Link to={`/notebooks/${notebookId}`}>
+            <button className="add-list__done-button">DONE</button>
+          </Link>
+        )}
+
+        {/* Edit an existing list => coming from list page*/}
+        {location.pathname.endsWith("/edit") && (
+          <Link to={`/notebooks/${notebookId}/lists/${listId}`}>
+            <button className="add-list__done-button">DONE</button>
+          </Link>
+        )}
+      </main>
+    </article>
   );
 }
